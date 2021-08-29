bc7197c9ddf308599bdfc8efa4762ec05afca30f
dnieto <davidnieto09@gmail.com>
2021-08-29 21:29:49 +0200

    american guy commit

--- src/test/java/FrameworkTutorialPart3_22/Mavenjava/RESTAPITest.java
+++ src/test/java/FrameworkTutorialPart3_22/Mavenjava/RESTAPITest.java
@@ -9,6 +9,13 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		//asdasd
+		//asdasdasdasd
+		//asdasdasdasdsdasd
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
 	}
 	
 	@Test
